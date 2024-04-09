9fd66c1b9caf134a3e60f65ee1cfc8fa2b03378f
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-04-09 20:41:05 +0200

    .

---
+++ src/app/createPlayer-component/createPlayer.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CreatePlayerComponent } from './createPlayer.component';
+
+describe('CreatePlayerComponent', () => {
+  let component: CreatePlayerComponent;
+  let fixture: ComponentFixture<CreatePlayerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreatePlayerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreatePlayerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/createPlayer-component/createPlayer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createPlayer-component',
+  templateUrl: './createPlayer.component.html',
+  styleUrls: ['./createPlayer.component.css']
+})
+export class CreatePlayerComponent {
+  // Aquí puedes definir propiedades y métodos para tu componente
+}
