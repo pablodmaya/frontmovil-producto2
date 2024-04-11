feb7d0152960536795a4dc908e5dec729e0e0a82
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-04-11 10:38:03 +0200

    .

--- src/app/components/createPlayer_component/createPlayer_component.component.spec.ts
+++ src/app/components/createPlayer_component/createPlayer_component.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CreatePlayerComponentComponent } from './createPlayer_component.component';
+import { CreatePlayerComponent } from './createPlayer_component.component';
 
 describe('CreatePlayerComponent', () => {
-  let component: CreatePlayerComponentComponent;
-  let fixture: ComponentFixture<CreatePlayerComponentComponent>;
+  let component: CreatePlayerComponent;
+  let fixture: ComponentFixture<CreatePlayerComponent>;
 });
--- src/app/components/createPlayer_component/createPlayer_component.component.ts
+++ src/app/components/createPlayer_component/createPlayer_component.component.ts
@@ -8,14 +8,77 @@ import { Storage, ref, uploadBytes } from '@angular/fire/storage';
   styleUrls: ['./createPlayer_component.component.css']
 })
 
-export class CreatePlayerComponentComponent {
-  constructor(private storage: Storage) {} // Assuming you inject Storage in constructor
+export class CreatePlayerComponent {
 
-  uploadImage($event: any){
-    const file = $event.target.files[0];
-    const imgRef = ref(this.storage, `assets/images/${file.name}`);
-    uploadBytes(imgRef, file)
-    .then(res => console.log(res))
-    .catch(e => console.log(e))
+  imgFileSelected = false;
+  videoFileSelected = false;
+  imgFile: File | undefined;
+  videoFile: File | undefined;
+  imgRef: any;
+  videoRef: any;
+
+  constructor(private storage: Storage) {}
+  onFileImgUpload($event: any){
+    this.imgFile = $event.target.files[0];
+    if(this.imgFile != undefined){
+      const fileType = this.imgFile.type;
+      if (fileType !== 'image/png' && fileType !== 'image/jpeg') {
+        console.error("Invalid file type. Only PNG and JPEG images are allowed.");
+        return;
+      }
+      this.imgRef = ref(this.storage, `assets/images/${this.imgFile.name}`);
+      this.imgFileSelected = true;      
+    }else{
+      this.imgFile = undefined;
+      this.imgRef = undefined;
+      this.imgFileSelected = false;
+    }
+  }
+
+  onFileVideoUpload($event: any){
+    this.videoFile = $event.target.files[0];
+    if(this.videoFile != undefined){
+      const fileType = this.videoFile.type;
+      if (fileType !== 'video/mp4') {
+        console.error("Invalid file type. Only MP4 videos are allowed.");
+        return;
+      }
+      this.videoRef = ref(this.storage, `assets/video/${this.videoFile.name}`);
+      this.videoFileSelected = true;      
+    }else{
+      this.videoFile = undefined;
+      this.videoRef = undefined;
+      this.videoFileSelected = false;
+    }
+  }
+
+  uploadImages(){
+    if (!this.imgFile || !this.imgRef) {
+      console.error("File or imgRef not available");
+      return;
+    }
+    uploadBytes(this.imgRef, this.imgFile)
+      .then(res => {
+        console.log("Upload successful:", res);
+        this.imgFile = undefined;
+        this.imgRef = undefined;
+        this.imgFileSelected = false;
+      })
+      .catch(e => console.error("Upload error:", e))
+  }
+
+  uploadVideo(){
+    if (!this.videoFile || !this.videoRef) {
+      console.error("File or imgRef not available");
+      return;
+    }
+    uploadBytes(this.videoRef, this.videoFile)
+      .then(res => {
+        console.log("Upload successful:", res);
+        this.videoFile = undefined;
+        this.videoRef = undefined;
+        this.videoFileSelected = false;
+      })
+      .catch(e => console.error("Upload error:", e))
   }
 }
