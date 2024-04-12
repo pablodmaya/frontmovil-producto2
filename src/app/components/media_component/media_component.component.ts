b4fafe61e822c34d00f8a7bfdc2cbce30829dea1
ifloresr7 <151829949+ifloresr7@users.noreply.github.com>
2024-04-12 09:28:18 +0200

    .

--- src/app/components/media_component/media_component.component.ts
+++ src/app/components/media_component/media_component.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {NgIf, UpperCasePipe} from '@angular/common';
 import { PlayerInterface } from '../../interfaces/playerInterface';
 
@@ -11,8 +11,16 @@ import { PlayerInterface } from '../../interfaces/playerInterface';
 })
 
 export class MediaComponentComponent {
+  
   @Input() player?: PlayerInterface;
   @Output() closeMedia = new EventEmitter();
+  @ViewChild('media') media!: ElementRef;
+  playVid() {
+    this.media.nativeElement.play();
+  }
+  pauseVid() {
+    this.media.nativeElement.pause();
+  }
   closeMediaModal() {
     this.closeMedia.emit(false);
   }
